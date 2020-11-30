d591d6616d1ffcb8b0fe1817492aab9051459e8a
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 06:41:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(path)
   YAML.load_file(path).each do |key, value|
   final_hash[key] = {}
   final_hash[key][:english] = value[0]
-  final_hash[key][:japanese] = ""
+  final_hash[key][:japanese] = value[1]
   binding.pry
 end
   final_hash
