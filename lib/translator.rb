37e4672c229d843bd1eebe15db23a1f034fb8462
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 06:48:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,8 +23,7 @@ def get_english_meaning(path, emoticon)
   if value[:japanese] == emoticon
     return key
   end
-  
-  
-  end
+end
+return "Sorry, that emoticon was not found"
   # code goes here
 end
